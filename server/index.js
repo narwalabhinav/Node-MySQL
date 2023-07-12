7ea729ebaa215228eb1c150444fb63ae62f1a381
ABHINAV NARWAL <72136278+narwalabhinav@users.noreply.github.com>
2023-07-12 15:39:13 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 import express from 'express'
 import mysql from 'mysql'
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 const app = express()
+dotenv.config()
 
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Abhi#123",
+    password: process.env.PASSWORD,
     database: "test"
 })
 
@@ -70,4 +72,5 @@ app.put("/books/:id", (req, res) => {
 
 app.listen(3000, () => {
     console.log("Connected to server!")
+    console.log(process.env.PASSWORD)
 })
